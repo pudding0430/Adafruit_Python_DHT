e8e35b7ea21e496de5004753eb5801de59553f57
pudding0430 <a46567779@gmail.com>
2018-11-27 20:42:44 +0800

    AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -20,10 +20,29 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import sys
-
+import time
 import Adafruit_DHT
-
-
+import httplib, urllib
+import json
+deviceId = "DpQUAGQx"
+deviceKey = "FSe2y0pELSD4qjzq"
+def post_to_mcs(payload):
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			conn = httplib.HTTPConnection("api.mediatek.com:80")
+			conn.connect() 
+			not_connected = 0 
+		except (httplib.HTTPException, socket.error) as ex: 
+			print "Error: %s" % ex
+			time.sleep(10)
+			# sleep 10 seconds 
+	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = conn.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	conn.close() 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
@@ -47,8 +66,15 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+while True:
+	h0, t0= Adafruit_DHT.read_retry(11, 4)
+	if h0 is not None and t0 is not None:
+		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
+		payload = {"datapoints":[{"dataChnId":"puddingH","values":{"value":h0}},
+		{"dataChnId":"puddingT","values":{"value":t0}}]} 
+		post_to_mcs(payload)
+		time.sleep(10) 
 else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
+	print('Failed to get reading. Try again!')
+	sys.exit(1)
+
